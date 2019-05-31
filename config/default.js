fe93fc8e01c4deaba1edf00abc603196e89afd65
tony <anthony.kerz@gmail.com>
2019-05-31 15:34:29 -0400

    refresh and ssl

--- config/default.js
+++ config/default.js
@@ -1,9 +1,9 @@
 module.exports = {
-  mongo: {
-    host: 'localhost:27017',
-    db: 'test-auto',
-    connectTimeoutMs: 3000,
-    socketTimeoutMs: 3000,
-    replicaSet: null
-  }
+	mongo: {
+		host: 'localhost:27017',
+		db: 'test-auto',
+		connectTimeoutMs: 3000,
+		socketTimeoutMs: 3000,
+		replicaSet: null
+	}
 }
--- test/ava/and-or-test.js
+++ test/ava/and-or-test.js
@@ -3,38 +3,38 @@ import test from 'ava'
 import {ensureAnd, pushOrs} from '../../src'
 
 test('getAnd', t => {
-  t.deepEqual(ensureAnd({}).$and, [])
-  t.deepEqual(ensureAnd({$and: ['foo']}).$and, ['foo'])
+	t.deepEqual(ensureAnd({}).$and, [])
+	t.deepEqual(ensureAnd({$and: ['foo']}).$and, ['foo'])
 })
 
 test('pushOrs', t => {
-  let query = {}
+	let query = {}
 
-  query = pushOrs({query, ors: [{foo: 'bar'}]})
-  t.deepEqual(query, {
-    $or: [{foo: 'bar'}]
-  })
+	query = pushOrs({query, ors: [{foo: 'bar'}]})
+	t.deepEqual(query, {
+		$or: [{foo: 'bar'}]
+	})
 
-  query = pushOrs({query, ors: [{baz: 'bip'}]})
-  t.deepEqual(query, {
-    $or: [{foo: 'bar'}],
-    $and: [
-      {
-        $or: [{baz: 'bip'}]
-      }
-    ]
-  })
+	query = pushOrs({query, ors: [{baz: 'bip'}]})
+	t.deepEqual(query, {
+		$or: [{foo: 'bar'}],
+		$and: [
+			{
+				$or: [{baz: 'bip'}]
+			}
+		]
+	})
 
-  query = pushOrs({query, ors: [{fee: 'fie'}]})
-  t.deepEqual(query, {
-    $or: [{foo: 'bar'}],
-    $and: [
-      {
-        $or: [{baz: 'bip'}]
-      },
-      {
-        $or: [{fee: 'fie'}]
-      }
-    ]
-  })
+	query = pushOrs({query, ors: [{fee: 'fie'}]})
+	t.deepEqual(query, {
+		$or: [{foo: 'bar'}],
+		$and: [
+			{
+				$or: [{baz: 'bip'}]
+			},
+			{
+				$or: [{fee: 'fie'}]
+			}
+		]
+	})
 })
